bd0ad2f0feea6e5a90ac629c74e68ba1b7180344
lgsh <1361441878@qq.com>
2021-12-22 18:05:15 +0800

    树型bean

---
+++ APlan-bean/src/main/java/com/aplan/bean/param/LabelValue.java
@@ -0,0 +1,16 @@
+package com.aplan.bean.param;
+
+import lombok.Data;
+
+/**
+ * @author lgsh
+ * @Description:
+ * @date 2021/10/27 22:16
+ */
+@Data
+public class LabelValue {
+
+        private String label;
+        private String value;
+
+}
